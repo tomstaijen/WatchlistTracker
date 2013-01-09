4c61f160586af9f92503d58a6f33a8ed1a187199
Tom Staijen <t.staijen@gmail.com>
2013-01-09 19:00:47 +0100

    dropbox api

---
+++ Source/WatchlistTracker/Logic/Dropbox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WatchlistTracker.Models;
+
+namespace WatchlistTracker.Logic
+{
+    public class Dropbox
+    {
+        public void Upload(User user, string filename, byte[] data)
+        {
+        }
+    }
+}
